72d86adbfcc6d25acbd00d0ef6bab08601fb9313
BalqeesKhalfan <b.k.malshuraqia@gmail.com>
2024-03-31 09:41:42 +0400

    initial Commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Create a Java class named Employee with private
+ * attributes for name, position, and salary.
+ * Write a method to display the employee's details.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
